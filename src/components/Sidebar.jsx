8641fb7186c02611967009cfbbe79335954f7b62
dilafar <it20784720@my.sliit.lk>
2022-10-15 11:04:09 +0530

    latest fix front models

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -127,7 +127,7 @@ const Sidebar = () => {
           </li>
         </ul>
         <p class="mb-14 px-5 py-3 hidden md:block text-center text-xs">
-          Copyright @2021
+          ChildrenCloud @2022
         </p>
       </div>
     </div>
